5000388255e9ceba6cec9ea4ec09336aa3c6a9d1
ray970111 <ray970111@gmail.com>
2026-01-14 09:14:54 +0800

    1/14

---
+++ Web/scripts/script.js
@@ -0,0 +1,19 @@
+let myAge = 20; //Sets a variable to my age
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(myAge);
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Ray".toLowerCase();
+console.log(myName);
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`
+);
